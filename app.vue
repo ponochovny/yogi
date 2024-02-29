7db7a8cc748396d10e1842c8f03c773bc63da229
ponochovny <ponochovny@gmail.com>
2024-02-29 20:48:54 +0200

    start

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+	<div :class="{ dark: darkMode }">
+		<img src="/img/logo.svg" alt="Yogi app" width="65" />
+		<h1>Yogi app!</h1>
+	</div>
+</template>
+<script setup>
+const darkMode = ref(false)
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class',
+	content: [],
+	theme: {
+		screens: {
+			xs: '320px',
+			sm: '614px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
+		extend: {
+			colors: {
+				// colors here
+			},
+		},
+	},
+	plugins: [],
+}
