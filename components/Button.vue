c7d655f652cd64bb6dfe7ce02a7368968bb64472
ponochovny <ponochovny@gmail.com>
2024-03-01 16:24:30 +0200

    update: button component type

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,7 @@
 <template>
 	<button
 		class="px-8 py-2 transition duration-300 ease-in-out bg-orange-600 rounded-full hover:bg-orange-600/75"
+		:type="props.type"
 	>
 		<slot />
 	</button>
@@ -13,4 +14,11 @@ export default defineComponent({
 	name: 'Button',
 })
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const props = defineProps({
+	type: {
+		type: String as PropType<'button' | 'submit'>,
+		default: 'button',
+	},
+})
+</script>
