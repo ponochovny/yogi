eb6f4f75c028a83efac85748d1d2ac33787385fa
ponochovny <ponochovny@gmail.com>
2024-03-01 16:25:56 +0200

    update: main page with comments

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-	<MainContainer>MainPage</MainContainer>
+	<div class="relative overflow-hidden min-h-screen">
+		<div
+			class="bg-[#93ACA8] w-[400px] h-[400px] rounded-full absolute -left-[200px] top-[300px]"
+		></div>
+		<div
+			class="bg-[#CA9A8E] w-[400px] h-[400px] rounded-full absolute -right-[200px] top-[800px]"
+		></div>
+		<MainContainer>
+			<!-- Search block -->
+			<!-- Slider with categories which link to search page -->
+			<!-- Popular locally list -->
+			<!-- Studios & Hosts list -->
+			<!-- Quote randomized -->
+			<!-- Upcoming virtual list -->
+			<!-- Wellness getaways list -->
+			<!-- Deeper Education list -->
+			<!-- Recommended for you list -->
+			<!-- Private sessions search -->
+			<!-- Subscribe form -->
+			<!-- Block with contacts & Site links -->
+		</MainContainer>
+	</div>
 </template>
 
 <script lang="ts">
