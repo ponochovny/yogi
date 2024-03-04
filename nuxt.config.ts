72d1d2442303585337859ff0eeaf55ad31b64d6f
ponochovny <ponochovny@gmail.com>
2024-03-04 23:14:01 +0200

    update: important for deploy / heroicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,10 @@ export default defineNuxtConfig({
 		componentDir: './components/ui',
 	},
 
+	build: {
+		transpile: ['@heroicons/vue'],
+	},
+
 	runtimeConfig: {
 		jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
 		jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
