2cad1b120c408cb45d83f07b35ebbf8358413000
ponochovny <ponochovny@gmail.com>
2024-03-09 00:05:56 +0200

    feat: ci, env example

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+	root: true,
+	extends: ['@nuxt/eslint-config'],
+	rules: {
+		'@typescript-eslint/no-unused-vars': 'off',
+		'vue/html-indent': ['error', 'tab'], // enforce tabs in template
+		indent: ['error', 'tab'], // enforce tabs in script and js files
+		'vue/max-attributes-per-line': 'off',
+		'vue/multi-word-component-names': 'off',
+		'vue/attributes-order': 'off',
+		'vue/no-reserved-component-names': 'off',
+		'vue/require-default-prop': 'off',
+		'vue/singleline-html-element-content-newline': 'off',
+		'vue/html-self-closing': 'off',
+		'vue/attribute-hyphenation': 'off',
+		'vue/no-unused-vars': 'off',
+		'no-async-promise-executor': 'off',
+	},
+}
