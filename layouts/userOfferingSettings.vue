0aed0c8a3a6c1559bc456ff7772d20ca278d1498
ponochovny <ponochovny@gmail.com>
2024-03-10 07:50:17 +0200

    feat: layouts

---
+++ layouts/userOfferingSettings.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="max-w-screen-xl pt-3 mx-auto">
+		<Header />
+		<div class="pt-10">
+			<div class="bg-white rounded-md flex">
+				<div class="pr-10 w-full max-w-[220px]">
+					<AdminSidebar :menu-items="_menuItems" />
+				</div>
+				<div class="pl-10 border-l pb-10 w-full">
+					<slot />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { userOfferingSettingsMenu } from '~/helpers/adminPanel'
+
+export default defineComponent({
+	name: 'UserAdminLayout',
+})
+</script>
+<script lang="ts" setup>
+const _menuItems = userOfferingSettingsMenu
+</script>
--- pages/user/Profile.vue
+++ pages/user/Profile.vue
@@ -1,56 +1,41 @@
 <template>
-	<div class="max-w-screen-xl pt-3 mx-auto">
-		<Header />
-		<div class="pt-10">
-			<div class="bg-white rounded-md flex">
-				<div class="pr-10 w-full max-w-[200px]">
-					<AdminSidebar :menu-items="_userMenu" />
+	<NuxtLayout name="user-admin">
+		<div class="max-w-[600px]">
+			<h1 class="pt-8 text-2xl font-bold mb-6">Edit profile</h1>
+			<div class="flex flex-col gap-3">
+				<div class="flex flex-col gap-2">
+					<span class="text-sm font-bold text-gray-700">Photo</span>
+					<img
+						:src="user?.profileImage || ''"
+						:alt="user?.name || ''"
+						width="100"
+						height="100"
+						class="rounded-full"
+					/>
 				</div>
-				<div class="pl-10 border-l pb-10 w-full max-w-[600px]">
-					<div class="max-w-[600px]">
-						<h1 class="pt-8 text-2xl font-bold mb-6">Edit profile</h1>
-						<div class="flex flex-col gap-3">
-							<div class="flex flex-col gap-2">
-								<span class="text-sm font-bold text-gray-700">Photo</span>
-								<img
-									:src="user?.profileImage || ''"
-									:alt="user?.name || ''"
-									width="100"
-									height="100"
-									class="rounded-full"
-								/>
-							</div>
-							<Input
-								v-model="data.email"
-								label="Your e-mail"
-								placeholder="Your e-mail"
-								type="email"
-							/>
-							<Input
-								v-model="data.name"
-								label="Your name"
-								placeholder="Your name"
-							/>
-							<Button
-								class="self-start"
-								@click="handleUpdateProfile"
-								:disabled="actionButtonDisabled"
-							>
-								<span class="text-white font-bold">Update</span>
-							</Button>
-						</div>
-					</div>
-				</div>
+				<Input
+					v-model="data.email"
+					label="Your e-mail"
+					placeholder="Your e-mail"
+					type="email"
+				/>
+				<Input v-model="data.name" label="Your name" placeholder="Your name" />
+				<Button
+					class="self-start"
+					@click="handleUpdateProfile"
+					:disabled="actionButtonDisabled"
+				>
+					<span class="text-white font-bold">Update</span>
+				</Button>
 			</div>
 		</div>
-	</div>
+	</NuxtLayout>
 </template>
 
 <script lang="ts">
 import { defineComponent, watch } from 'vue'
 import { toast } from 'vue-sonner'
 import type { IUser } from '~/server/types'
-import { userMenu } from '~/helpers/adminPanel'
 
 export default defineComponent({
 	name: 'ProfilePage',
@@ -63,7 +48,6 @@ definePageMeta({
 const { useAuthUser, updateProfile } = useAuth()
 
 const user = useAuthUser() as Ref<IUser>
-const _userMenu = userMenu
 const actionButtonDisabled = computed(() => {
 	if (!user.value) return true
 	const obj1 = { ...data }
