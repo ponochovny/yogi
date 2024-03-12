9e7fe09ae08ce7a8d4e4c8eab0efd8d4c71e6f84
ponochovny <ponochovny@gmail.com>
2024-03-12 08:15:30 +0200

    update: button component size prop

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,7 @@
 <template>
 	<button
-		class="px-8 py-2 transition duration-300 ease-in-out bg-orange-600 rounded-full hover:bg-orange-600/75 disabled:bg-orange-600/20 disabled:cursor-default"
+		class="flex items-center transition duration-300 ease-in-out bg-orange-600 rounded-full hover:bg-orange-600/75 disabled:bg-orange-600/20 disabled:cursor-default"
+		:class="cn([props.btnSize === 'md' ? 'px-8 py-2' : 'px-3 py-1'])"
 		:type="props.type"
 	>
 		<slot />
@@ -9,16 +10,19 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { cn } from '@/lib/utils'
 
 export default defineComponent({
 	name: 'Button',
 })
 </script>
 <script lang="ts" setup>
-const props = defineProps({
-	type: {
-		type: String as PropType<'button' | 'submit'>,
-		default: 'button',
-	},
+interface IProps {
+	type?: 'button' | 'submit'
+	btnSize?: 'sm' | 'md'
+}
+const props = withDefaults(defineProps<IProps>(), {
+	type: 'button',
+	btnSize: 'md',
 })
 </script>
