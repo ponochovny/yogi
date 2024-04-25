296f6fb8cdcc1bc2bca1256c522ecf5e66c40003
ponochovny <ponochovny@gmail.com>
2024-04-25 17:02:09 +0300

    feat: search api, cards

--- pages/Search.vue
+++ pages/Search.vue
@@ -1,6 +1,32 @@
 <template>
-	<SearchContainer @update="updated">
-		<div class="py-10 px-6">Search Page</div>
+	<SearchContainer @update="updated" :loading="loading">
+		<div class="py-10 px-6">
+			<div
+				class="grid xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 gap-x-4 gap-y-7 auto-rows-1fr"
+			>
+				<template v-if="filters.activityType === 'Offerings'">
+					<OfferingCard
+						v-for="offering of (searchResults as IOffering[])"
+						:key="offering?.id"
+						:offering="offering"
+					/>
+				</template>
+				<template v-if="filters.activityType === 'Studio & Event Hosts'">
+					<StudioCard
+						v-for="studio of (searchResults as IStudio[])"
+						:key="studio?.id"
+						:studio="studio"
+					/>
+				</template>
+				<template v-if="filters.activityType === 'Practitioners'">
+					<PractitionerCard
+						v-for="practitioner of (searchResults as IUser[])"
+						:key="practitioner?.id"
+						:practitioner="practitioner"
+					/>
+				</template>
+			</div>
+		</div>
 	</SearchContainer>
 </template>
 
@@ -8,14 +34,21 @@
 import { defineComponent } from 'vue'
 import { DATA_TYPES } from '~/helpers/constants'
 import type { ISearchParams } from '~/helpers/types/search'
+import { format } from 'date-fns'
+import type { IOffering } from '~/helpers/types/offering'
+import type { IStudio } from '~/helpers/types/studio'
+import type { IUser } from '~/server/types'
 
 export default defineComponent({
 	name: 'SearchPage',
 })
 </script>
 <script lang="ts" setup>
+const loading = ref(false)
+const searchResults = ref<IOffering[] | IStudio[] | IUser[]>([])
+const isInitialLoad = ref(true)
 const filters = reactive<ISearchParams>({
-	activeType: DATA_TYPES[0],
+	activityType: DATA_TYPES[0],
 	categories: [],
 	types: [],
 	date: {
@@ -23,12 +56,44 @@ const filters = reactive<ISearchParams>({
 		end: undefined,
 	},
 })
+function resetSearchResults() {
+	searchResults.value = []
+}
+
 function updated(data: any) {
-	if (JSON.stringify(data) !== JSON.stringify(filters)) {
-		console.log('updated', data)
-		filters.activeType = data.activeType
+	if (JSON.stringify(data) !== JSON.stringify(filters) || isInitialLoad.value) {
+		filters.activityType = data.activityType
 		filters.categories = data.categories
 		filters.types = data.types
+
+		resetSearchResults()
+		fetch()
 	}
+	if (isInitialLoad.value) isInitialLoad.value = false
+}
+
+function fetch() {
+	loading.value = true
+	$fetch<{ data: IOffering[] }>(
+		'/api/search?' +
+			new URLSearchParams({
+				...(filters.activityType && { activityType: filters.activityType }),
+				...(filters.categories.length && {
+					categories: filters.categories.join(','),
+				}),
+				...(filters.types.length && { types: filters.types.join(',') }),
+				...(filters.date.start && {
+					date_start: format(filters.date.start, 'yyyy-MM-dd'),
+				}),
+				...(filters.date.end && {
+					date_end: format(filters.date.end, 'yyyy-MM-dd'),
+				}),
+			}).toString()
+	)
+		.then(({ data }) => {
+			searchResults.value = data
+		})
+		.catch((error) => console.log(error))
+		.finally(() => (loading.value = false))
 }
 </script>
--- server/api/search/index.get.ts
+++ server/api/search/index.get.ts
@@ -1,5 +1,11 @@
+import type { TDataType } from '~/helpers/types/search'
 import { getOfferings } from '~/server/db/offerings'
+import { getPractitioners } from '~/server/db/practitioners'
+import { getStudios } from '~/server/db/studio'
 import { offeringTransformer } from '~/server/transformers/offering'
+import { studioTransformer } from '~/server/transformers/studio'
+import { practitionerTransformer } from '~/server/transformers/user'
+import type { IUser } from '~/server/types'
 
 const PAGE_COUNT_DEFAULT = 1
 const PER_PAGE_DEFAULT = 20
@@ -9,9 +15,19 @@ const DATE_START_DEFAULT = null
 const DATE_END_DEFAULT = null
 
 export default defineEventHandler(async (event) => {
-	const query = getQuery(event)
+	const query = getQuery(event) as {
+		name?: string
+		page?: number
+		count?: number
+		activityType?: TDataType
+		types?: string[]
+		categories?: string[]
+		date_start?: string
+		date_end?: string
+	}
 	const {
-		name,
+		name = '',
+		activityType: ACTIVITY_TYPE,
 		page: PAGE_COUNT = PAGE_COUNT_DEFAULT,
 		count: PER_PAGE = PER_PAGE_DEFAULT,
 		types: TYPES_QUERY = TYPES_QUERY_DEFAULT,
@@ -20,24 +36,19 @@ export default defineEventHandler(async (event) => {
 		date_end: DATE_END = DATE_END_DEFAULT,
 	} = query
 
-	if (typeof name !== 'string') {
-		throw createError({ status: 400, statusMessage: 'Query is incorrect' })
-	}
-
-	try {
-		const offeringsList = await getOfferings({
+	if (ACTIVITY_TYPE === 'Offerings') {
+		const offerings = await getOfferings({
 			include: {
 				banners: true,
 			},
 			where: {
 				name: {
-					contains: name,
+					contains: typeof name !== 'string' ? '' : name,
 					mode: 'insensitive',
 				},
 			},
 		})
-
-		const filteredOfferings = offeringsList
+		const filteredOfferings = offerings
 			.filter((offering) => {
 				return true
 			})
@@ -47,7 +58,66 @@ export default defineEventHandler(async (event) => {
 			data: filteredOfferings,
 			status: 'Success!',
 		}
-	} catch (error) {
-		throw createError({ status: 500 })
+	}
+
+	if (ACTIVITY_TYPE === 'Studio & Event Hosts') {
+		const studios = await getStudios({
+			include: {
+				owner: true,
+				logo: true,
+				banner: true,
+				practitioners: {
+					include: {
+						user: true,
+					},
+				},
+			},
+			where: {
+				name: {
+					contains: typeof name !== 'string' ? '' : name,
+					mode: 'insensitive',
+				},
+			},
+		})
+
+		return {
+			data: studios.map((studio) => studioTransformer(studio)),
+			status: 'Success!',
+		}
+	}
+
+	if (ACTIVITY_TYPE === 'Practitioners') {
+		const practitioners = await getPractitioners({
+			include: {
+				user: true,
+			},
+			where: {
+				user: {
+					name: {
+						contains: name,
+						mode: 'insensitive',
+					},
+				},
+			},
+		})
+		const pracs = [
+			...(
+				practitioners as unknown as {
+					id: string
+					userId: string
+					studioId: string
+					offeringId: string
+					user: IUser
+				}[]
+			).map((practitioner) => practitionerTransformer(practitioner.user)),
+		]
+		const uniquePractitionersList = [
+			...new Map(pracs.map((item: any) => [item.id, item])).values(),
+		]
+
+		return {
+			data: uniquePractitionersList,
+			status: 'Success!',
+		}
 	}
 })
