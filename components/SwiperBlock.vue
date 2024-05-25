d15a290778b2c1d23d8ae8054ba06d4fd39d6a11
ponochovny <ponochovny@gmail.com>
2024-05-26 02:34:48 +0300

    fix: swiper block width

--- components/SwiperBlock.vue
+++ components/SwiperBlock.vue
@@ -1,33 +1,35 @@
 <template>
-	<div class="relative w-full px-5" v-if="length">
-		<div class="rounded-2xl overflow-hidden">
-			<Swiper
-				@swiper="onSwiper"
-				@snapIndexChange="snapIndexChange"
-				:modules="[SwiperAutoplay, SwiperNavigation]"
-				slides-per-view="auto"
-				:space-between="24"
-				:navigation="{
-					nextEl: nextNavigation,
-					prevEl: prevNavigation,
-				}"
+	<div class="-mx-5" v-if="length">
+		<div class="relative w-full px-5">
+			<div class="rounded-2xl overflow-hidden">
+				<Swiper
+					@swiper="onSwiper"
+					@snapIndexChange="snapIndexChange"
+					:modules="[SwiperAutoplay, SwiperNavigation]"
+					slides-per-view="auto"
+					:space-between="24"
+					:navigation="{
+						nextEl: nextNavigation,
+						prevEl: prevNavigation,
+					}"
+				>
+					<slot />
+				</Swiper>
+			</div>
+			<div
+				ref="prevNavigation"
+				class="swiper-button-prev-my absolute top-1/2 -translate-y-1/2 left-0 z-10 w-10 h-10 bg-white shadow-md rounded-full flex justify-center items-center hover:bg-gray-100 active:bg-gray-50 transition-all cursor-pointer"
+				:class="{ 'opacity-0': isBeginning && !isMiddle }"
 			>
-				<slot />
-			</Swiper>
-		</div>
-		<div
-			ref="prevNavigation"
-			class="swiper-button-prev-my absolute top-1/2 -translate-y-1/2 left-0 z-10 w-10 h-10 bg-white shadow-md rounded-full flex justify-center items-center hover:bg-gray-100 active:bg-gray-50 transition-all cursor-pointer"
-			:class="{ 'opacity-0': isBeginning && !isMiddle }"
-		>
-			<ChevronLeftIcon class="w-5 h-5 text-gray-600 stroke-1" />
-		</div>
-		<div
-			ref="nextNavigation"
-			class="swiper-button-next-my absolute top-1/2 -translate-y-1/2 right-0 z-10 w-10 h-10 bg-white shadow-md rounded-full flex justify-center items-center hover:bg-gray-100 active:bg-gray-50 transition-all cursor-pointer"
-			:class="{ 'opacity-0': isEnd && !isMiddle }"
-		>
-			<ChevronRightIcon class="w-5 h-5 text-gray-600 stroke-1" />
+				<ChevronLeftIcon class="w-5 h-5 text-gray-600 stroke-1" />
+			</div>
+			<div
+				ref="nextNavigation"
+				class="swiper-button-next-my absolute top-1/2 -translate-y-1/2 right-0 z-10 w-10 h-10 bg-white shadow-md rounded-full flex justify-center items-center hover:bg-gray-100 active:bg-gray-50 transition-all cursor-pointer"
+				:class="{ 'opacity-0': isEnd && !isMiddle }"
+			>
+				<ChevronRightIcon class="w-5 h-5 text-gray-600 stroke-1" />
+			</div>
 		</div>
 	</div>
 </template>
