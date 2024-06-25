3c2bc9852918fac09f2848906980309647b04ec1
ponochovny <ponochovny@gmail.com>
2024-06-26 02:25:38 +0300

    refactor: offering creation form with validation & clean code

--- components/offering/Creation/Index.vue
+++ components/offering/Creation/Index.vue
@@ -1,11 +1,15 @@
 <template>
 	<div class="flex flex-col gap-3 lg:pr-0 pr-6 max-w-[600px]">
-		<div>
-			<pre class="text-xs">{{ values }}</pre>
-			<pre class="text-xs">{{ errors }}</pre>
-		</div>
-		<form @submit.prevent="onSubmit" class="flex flex-col gap-4 py-4">
-			<Input v-model="name" label="Offering name" placeholder="Offering name" />
+		<form @submit.prevent="onSubmit" class="flex flex-col gap-4">
+			<Field v-model="name" name="name" v-slot="{ field, errorMessage }">
+				<Input
+					v-bind="field"
+					v-model="name"
+					label="Offering name"
+					placeholder="Offering name"
+					:error="errorMessage"
+				/>
+			</Field>
 			<div class="flex gap-2 flex-col sm:flex-row">
 				<Datepicker v-if="start" label="Start" v-model="start" />
 				<Datepicker v-if="end" label="End" v-model="end" />
@@ -13,25 +17,55 @@
 			<Yselect
 				v-model="activity"
 				label="Activity type"
-				:options="['Class', 'Event', 'Appointment']"
+				field="label"
+				valueProp="value"
+				:options="[
+					{
+						value: 'class',
+						label: 'Class',
+					},
+					{
+						value: 'event',
+						label: 'Event',
+					},
+					{
+						value: 'appointment',
+						label: 'Appointment',
+					},
+				]"
 			/>
 
 			<div class="flex gap-3 sm:gap-2 flex-col sm:flex-row">
-				<Input
-					:modelValue="duration"
-					@update:modelValue="duration = Number($event)"
-					label="Offering duration"
-					placeholder="Offering duration"
-					type="number"
-				/>
-				<Input
-					:modelValue="spots"
-					@update:modelValue="spots = Number($event)"
-					label="Offering spots"
-					placeholder="Offering spots"
-					type="number"
-				/>
-				<div class="flex space-x-2 sm:pb-3 self-end items-center mr-auto">
+				<Field
+					v-model="duration"
+					name="duration"
+					v-slot="{ field, errorMessage }"
+				>
+					<Input
+						v-bind="field"
+						:modelValue="duration"
+						@update:modelValue="duration = Number($event)"
+						label="Offering duration"
+						placeholder="Offering duration"
+						type="number"
+						:error="errorMessage"
+					/>
+				</Field>
+
+				<Field v-model="spots" name="spots" v-slot="{ field, errorMessage }">
+					<Input
+						v-bind="field"
+						:modelValue="spots"
+						@update:modelValue="spots = Number($event)"
+						label="Offering spots"
+						placeholder="Offering spots"
+						type="number"
+						:error="errorMessage"
+					/>
+				</Field>
+				<div
+					class="flex space-x-2 sm:pb-3 self-start md:mt-8 items-center mr-auto"
+				>
 					<Switch
 						id="is_private"
 						:checked="is_private"
@@ -47,41 +81,61 @@
 				placeholder="Offering description"
 			/>
 			<div class="flex gap-3 sm:gap-2 flex-col sm:flex-row">
-				<Yselect
+				<Field
 					v-model="categories"
-					label="Categories"
-					:options="_categories"
-					field="name"
-					value-prop="name"
-					mode="multiple"
-				/>
-				<Yselect
-					v-model="types"
-					label="Types"
-					:options="_types"
-					field="name"
-					value-prop="name"
-					mode="multiple"
-				/>
+					name="categories"
+					v-slot="{ field, errorMessage }"
+				>
+					<Yselect
+						v-bind="field"
+						v-model="categories"
+						label="Categories"
+						:options="_categories"
+						field="name"
+						value-prop="name"
+						mode="multiple"
+						:error="errorMessage"
+					/>
+				</Field>
+				<Field v-model="types" name="types" v-slot="{ field, errorMessage }">
+					<Yselect
+						v-bind="field"
+						v-model="types"
+						label="Types"
+						:options="_types"
+						field="name"
+						value-prop="name"
+						mode="multiple"
+						:error="errorMessage"
+					/>
+				</Field>
 			</div>
 			<div class="flex gap-3 sm:gap-2 flex-col sm:flex-row">
-				<Yselect
-					label="Offering location"
-					ref="selectComponent"
-					class="w-full"
-					mode="single"
-					v-model="_location"
-					field="place_name"
-					valueProp="id"
-					object
-					:options="features"
-					placeholder="Find place on the map"
-					:disabled="!features.length"
-				/>
+				<Field
+					v-model="location"
+					name="location"
+					v-slot="{ field, errorMessage }"
+				>
+					<Yselect
+						v-bind="field"
+						label="Offering location"
+						ref="selectComponent"
+						class="w-full"
+						mode="single"
+						v-model="_location"
+						field="place_name"
+						valueProp="id"
+						object
+						:options="features"
+						placeholder="Find place on the map"
+						:disabled="!features.length"
+						:error="errorMessage"
+					/>
+				</Field>
 				<button
-					v-if="_location?.place_name"
+					v-if="_location?.place_name && updateData"
 					@click="resetMarker(true)"
-					class="flex items-center justify-center h-[42px] self-end rounded-md border border-gray-300 px-2 bg-white hover:bg-gray-100 shadow-sm mr-auto"
+					class="flex items-center justify-center h-[42px] self-start mt-7 rounded-md border border-gray-300 px-2 bg-white hover:bg-gray-100 shadow-sm mr-auto"
 					title="Reset"
 					type="button"
 				>
@@ -89,7 +143,7 @@
 				</button>
 				<button
 					@click="isShowMap = !isShowMap"
-					class="flex items-center justify-center h-[42px] self-end rounded-md border border-gray-300 px-2 bg-white hover:bg-gray-100 shadow-sm mr-auto"
+					class="flex items-center justify-center h-[42px] self-start mt-7 rounded-md border border-gray-300 px-2 bg-white hover:bg-gray-100 shadow-sm mr-auto"
 					title="Open map"
 					type="button"
 				>
@@ -110,7 +164,7 @@
 					ref="map"
 					allowMarkerCreation
 					searchable
-					:center="getCenter()"
+					:center="getCenter(offering)"
 					:sMarker="getMarkers()"
 					:zoom="updateData ? 12 : 3"
 					@features="setFeatures"
@@ -120,17 +174,21 @@
 			</div>
 
 			<!-- PRACTITIONERS SELECT -->
-			<Yselect
-				mode="tags"
-				v-model="_practitioners"
-				label="Practitioners"
-				:options="practitionersOptions"
-				object
-				placeholder="Select practitioners"
-				searchable
-				field="name"
-				value-prop="name"
-			/>
+			<Field name="practitioners" v-slot="{ field, errorMessage }">
+				<Yselect
+					v-bind="field"
+					mode="tags"
+					v-model="_practitioners"
+					label="Practitioners"
+					:options="practitionersOptions"
+					object
+					placeholder="Select practitioners"
+					searchable
+					field="name"
+					value-prop="name"
+					:error="errorMessage"
+				/>
+			</Field>
 
 			<OfferingTicketsList
 				v-if="tickets"
@@ -154,9 +212,9 @@
 				@change="moduleImagesChange"
 			/>
 
-			<ul class="text-rose-600">
+			<!-- <ul class="text-rose-600">
 				<li v-for="(error, key) in errors" :key="key">{{ error }}</li>
-			</ul>
+			</ul> -->
 
 			<div class="flex gap-2">
 				<Button class="self-start">Submit</Button>
@@ -182,45 +240,81 @@ import {
 	offeringDataCreationSchemaTyped,
 	type TOfferingCreationData,
 } from '~/helpers/types/offering'
+import type { TOffering } from '~/helpers/types/offering'
 import { ArrowPathIcon, MapIcon } from '@heroicons/vue/24/outline'
 import { toast } from 'vue-sonner'
 import timezones from '~/helpers/timeZones.json'
 import type { TMarker } from '~/helpers/types/map'
+import type { TTicket } from '~/helpers/types/ticket'
+import type { TTicketCreate } from '~/helpers/types/offering'
+import type { TPractitioner } from '~/server/types'
 import useBanners from './helpers/useBanners'
 import useLocation from './helpers/useLocation'
 import usePractitioners from './helpers/usePractitioners'
 import useTickets from './helpers/useTickets'
 
 export default defineComponent({
-	name: 'TestCreationForm',
+	name: 'CreationForm',
 })
 </script>
 <script lang="ts" setup>
-const updateData = ref(false)
+interface IProps {
+	updateData?: boolean
+	offering?: TOffering | null
+	// studioId?: string
+}
+const props = defineProps<IProps>()
+const router = useRouter()
+
+// const studioId = computed(() => {
+// 	const {id} = route.params
+// 	return isArray(id)
+// 			? (id[0] as string)
+// 			: (id as string),
+
+// })
+
+// const offeringLocation = {
+// 	center: [0, 0],
+// 	id: props.offering?.location?.name || '',
+// 	place_name: props.offering?.location?.name || '',
+// }
+const initialLocation = {
+	coords: [0, 0],
+	name: '',
+}
+
+const transformedTicket = (ticket: TTicket): TTicketCreate => {
+	return {
+		id: ticket.id,
+		name: ticket.name,
+		description: ticket.description,
+		price: ticket.price_int / 100,
+		currency: ticket.currency,
+		status: ticket.status,
+	}
+}
 const _categories = _data.categories
 const _types = _data.types
 const _timezones = timezones
 const { values, meta, errors, handleSubmit, resetForm, defineField } = useForm({
 	validationSchema: offeringDataCreationSchemaTyped,
 	initialValues: {
-		name: '',
-		start: new Date(),
-		end: new Date(),
-		activity: EActivity.CLASS,
+		name: props.offering?.name || '',
+		start: new Date(props.offering?.start || new Date()),
+		end: new Date(props.offering?.end || new Date()),
+		activity: (props.offering?.activity as EActivity) || EActivity.CLASS,
 		banners: [],
-		duration: 0,
-		description: '',
-		spots: 0,
-		is_private: false,
-		categories: [],
-		types: [],
-		location: {
-			coords: [0, 0],
-			name: '',
-		},
-		timezone: _timezones[0].tzId,
-		practitioners: [],
-		tickets: [
+		duration: props.offering?.duration || 0,
+		description: props.offering?.description || '',
+		spots: props.offering?.spots || 0,
+		is_private: props.offering?.is_private || false,
+		categories: props.offering?.categories || [],
+		types: props.offering?.types || [],
+		location: initialLocation,
+		timezone: props.offering?.timezone || _timezones[0].tzId,
+		practitioners: props.offering?.practitioners.map((el) => el.id) || [],
+		tickets: props.offering?.tickets.map((el) => transformedTicket(el)) || [
 			{
 				name: '',
 				description: '',
@@ -229,9 +323,9 @@ const { values, meta, errors, handleSubmit, resetForm, defineField } = useForm({
 				status: 'active',
 			},
 		],
-		isActive: false,
-		slug: '',
-		studioId: '65f9cb054cf3fa686ab8a90a',
+		isActive: props.offering?.isActive || false,
+		slug: props.offering?.slug || '',
+		studioId: props.offering?.studio?.id || '65f9cb054cf3fa686ab8a90a',
 	},
 })
 
@@ -250,6 +344,30 @@ const [location] = defineField('location')
 const [practitioners] = defineField('practitioners')
 const [tickets] = defineField('tickets')
 
+onBeforeMount(() => {
+	const center = (location?: TMarker) => {
+		if (!location) {
+			return {
+				center: [0, 0],
+				id: '',
+				place_name: '',
+			}
+		}
+		return {
+			center: [location.coords[1], location.coords[0]],
+			id: location.name,
+			place_name: location.name,
+		}
+	}
+	_practitioners.value = props.offering?.practitioners || []
+	_location.value = center(props.offering?.location)
+	bannersUpdate.value =
+		props.offering?.banners.map((el) => ({
+			id: el.id || '',
+			url: el.url,
+		})) || []
+})
+
 const moduleImagesUploader = ref<any>(null)
 const {
 	bannersUpdate,
@@ -257,7 +375,7 @@ const {
 	bannersOrder,
 	bannersFiles,
 	moduleImagesChange,
-} = useBanners(updateData.value)
+} = useBanners(props.updateData, props.offering?.banners)
 
 const {
 	_location,
@@ -270,7 +388,7 @@ const {
 	featureSelected,
 	markerRemoved,
 	resetLocation,
-} = useLocation(location as Ref<TMarker | null>, updateData.value)
+} = useLocation(location as Ref<TMarker | null>, props.updateData)
 
 const { addEmptyTicket, removeTicket } = useTickets(tickets)
 
@@ -298,88 +416,128 @@ const { createOffering, updateOffering } = useOffering()
 const onSubmit = handleSubmit((values) => {
 	// alert(JSON.stringify(values, null, 2))
 
-	if (updateData.value) {
-		// updateOfferingHandler()
+	if (props.updateData) {
+		updateOfferingHandler(values)
 	} else {
 		createOfferingHandler(values)
 	}
 })
 
 async function createOfferingHandler(values: TOfferingCreationData) {
-	createOffering({ ...values, banners: bannersFiles.value })
-		.then(() => {
+	console.log({ ...values, banners: bannersFiles.value })
+	// toast.success()
+	return createOffering({ ...values, banners: bannersFiles.value })
+		.then((res: any) => {
+			console.log('res', res)
 			resetFormHandler()
 			toast.success(
 				'Offering has been created successfully. Currently not active'
 			)
+			setTimeout(() => {
+				toast.success('You will be redirected to the offering page')
+			}, 2000)
+			setTimeout(() => {
+				router.push('/offering/' + res.data.slug)
+			}, 4000)
 		})
 		.catch((error) => {
 			console.log(error)
 			toast.error(error)
 		})
 }
-// async function updateOfferingHandler() {
-// 	if (!props.offering?.id) return
 
-// 	const oldPracs = props.offering.practitioners.map((el) => el.id)
-// 	const updatedPracs = formData.practitioners.map((el) => el.id)
-// 	const practitionersRemove = getDifference(oldPracs, updatedPracs)
-// 	const newPracs = updatedPracs.filter(
-// 		(el) => !oldPracs.some((_el) => _el === el)
-// 	)
+const updatePractitionersData = (
+	practitioners: TPractitioner[],
+	collectedPractitioners: any[]
+) => {
+	function getDifference(oldArray: string[], newArray: string[]) {
+		return oldArray.filter((x) => !newArray.includes(x))
+	}
+	const oldPracs = practitioners.map((el) => el.id)
+	const updatedPracs = collectedPractitioners.map((el) => el.id)
+	const practitionersRemove = getDifference(oldPracs, updatedPracs)
+	const newPracs = updatedPracs.filter(
+		(el) => !oldPracs.some((_el) => _el === el)
+	)
 
-// 	const oldTickets = props.offering.tickets?.map((el) => el.id) || []
-// 	const updatedTickets = formData.tickets.map((el) => el?.id || '')
-// 	const newTickets = formData.tickets.filter((el) => !el.id)
-// 	const ticketsRemove = getDifference(oldTickets, updatedTickets)
+	return { practitionersRemove, newPracs }
+}
+const updateTicketsData = (
+	oldTickets: TTicket[],
+	collectedTickets: TTicketCreate[]
+) => {
+	function getDifference(oldArray: string[], newArray: string[]) {
+		return oldArray.filter((x) => !newArray.includes(x))
+	}
+	const _oldTickets = oldTickets?.map((el) => el.id) || []
+	const updatedTickets = collectedTickets.map((el) => el?.id || '')
+	const newTickets: TTicketCreate[] = collectedTickets.filter((el) => !el.id)
+	const ticketsRemove = getDifference(_oldTickets, updatedTickets)
 
-// 	const deepCloned = useCloneDeep({
-// 		...formData,
-// 		name: formData.name,
-// 		activity: formData.activity.toLowerCase() as
-// 			| 'appointment'
-// 			| 'class'
-// 			| 'event',
-// 		location: {
-// 			coords: formData.location
-// 				? [formData.location?.center[1], formData.location?.center[0]]
-// 				: [],
-// 			name: formData.location?.place_name || '',
-// 		},
-// 		duration: +formData.duration,
-// 		spots: +formData.spots,
-// 		start: new Date(formData.start),
-// 		end: new Date(formData.end),
-// 		bannersDelete: bannersDelete.value,
-// 		bannersOrder: bannersOrder.value,
-// 		banners: bannersFiles.value,
-// 		studioId: isArray(studioId)
-// 			? (studioId[0] as string)
-// 			: (studioId as string),
-// 		tickets: newTickets,
-// 		ticketsRemove,
-// 		practitioners: newPracs,
-// 		practitionersRemove,
-// 		isActive: props.offering.isActive,
-// 	})
-// 	updateOffering(deepCloned, props.offering.id)
-// 		.then(() => {
-// 			emit('updated')
-// 			toast.success(
-// 				`Offering data has been updated. ${
-// 					practitionersRemove.length
-// 						? practitionersRemove.length + ' practitioner(s) removed.'
-// 						: ''
-// 				} ${
-// 					ticketsRemove.length
-// 						? ticketsRemove.length + ' ticket(s) set inactive.'
-// 						: ''
-// 				}`
-// 			)
-// 		})
-// 		.catch((error: any) => {
-// 			console.log(error)
-// 			toast.error('Error ocurred')
-// 		})
-// }
+	return {
+		newTickets,
+		ticketsRemove,
+	}
+}
+async function updateOfferingHandler(values: TOfferingCreationData) {
+	if (
+		!props.offering?.id
+		// || !props.studioId
+	)
+		return
+
+	// Practitioners
+	const { newPracs, practitionersRemove } = updatePractitionersData(
+		props.offering.practitioners,
+		_practitioners.value
+	)
+
+	// Tickets
+	const { newTickets, ticketsRemove } = updateTicketsData(
+		props.offering.tickets,
+		tickets.value || []
+	)
+
+	console.log({
+		...values,
+		tickets: newTickets,
+		ticketsRemove,
+		practitioners: newPracs,
+		practitionersRemove,
+		banners: bannersFiles.value,
+		bannersDelete: bannersDelete.value,
+		bannersOrder: bannersOrder.value,
+
+		offeringId: props.offering.id,
+	})
+
+	return updateOffering(
+		{
+			...values,
+			tickets: newTickets,
+			ticketsRemove,
+			practitioners: newPracs,
+			practitionersRemove,
+			banners: bannersFiles.value,
+			bannersDelete: bannersDelete.value,
+			bannersOrder: bannersOrder.value,
+		},
+		props.offering.id
+	)
+		.then(() => {
+			const pracsMsg = practitionersRemove.length
+				? practitionersRemove.length + ' practitioner(s) removed.'
+				: ''
+			const ticketsMsg = ticketsRemove.length
+				? ' ' + ticketsRemove.length + ' ticket(s) set inactive.'
+				: ''
+			const sumMsg = pracsMsg + ticketsMsg
+
+			toast.success(`Offering data has been updated. ${sumMsg}`)
+		})
+		.catch((error: any) => {
+			console.log(error)
+			toast.error(error.message)
+		})
+}
 </script>
